5a5c1c6dca2dc204bc702aefe64398fcd385c18c
Ernesto Boca <boca.ernesto13@gmail.com>
2025-07-30 17:45:20 +0200

    Task 0 fixxxxxXX

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,3 +1,3 @@
-Book.objects.filter(author__name=author_name)
 Library.objects.get(name=library_name).books.all()
+Author.objects.get(name=author_name)
 Library.objects.get(name=library_name).librarian
