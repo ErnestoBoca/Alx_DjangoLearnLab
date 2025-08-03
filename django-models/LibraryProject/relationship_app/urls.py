1cdfa7c99ca5c8ad6eb5afd63d6909b8fb7fdbbd
Ernesto Boca <boca.ernesto13@gmail.com>
2025-08-03 18:38:31 +0200

    login implemented

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import list_books
-from .views import LibraryDetailView
+from .views import list_books, LibraryDetailView, register_view, home_view, login_view, logout_view
+
 
 urlpatterns = [
-    path('list/', views.list_books, name='list-books'),
-    path('library/', views.LibraryDetailView.as_view(), name='libray-detail')
+    path('list/', list_books, name='list-books'),
+    path('library/', LibraryDetailView.as_view(), name='libray-detail'),
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('', home_view, name='home')
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Book
 from .models import Library
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def list_books(request):
     books = Book.objects.all()
@@ -19,3 +22,38 @@ class LibraryDetailView(ListView):
 
     def get_queryset(self):
         return Library.objects.get(name='UEM-CE')
+
+def home_view(request):
+    return render(request, 'relationship_app/home.html')
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Registration successful! You can now log in.')
+            return redirect('login')
+        else:
+            messages.error(request, 'Please correct the errors below.')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'relationship_app/register.html', {'form':form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'relationship_app/login.html', {'form':form})
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'You have been logged out')
+    return redirect('login')
